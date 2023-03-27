9f35f025db64f21fd37989e8ac7959f4edc6af21
abdolazizsalimi <aziz.s6882@gmail.com>
2023-03-27 22:04:12 +0430

    TODO  :[KNN TODO]

---
+++ knn.py
@@ -0,0 +1,14 @@
+
+
+
+class KNN:
+
+    def __init__(self , k = 3 , ):
+        self.k = k 
+
+    def fit(self , X,y):
+        pass
+
+    def predict(self, X):
+        self.x = x 
+        pass
